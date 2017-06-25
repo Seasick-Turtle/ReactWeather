8d4ffc06bcf78c13d3a001f229429b9ba96b8913
Christopher Martin <martincdev@gmail.com>
2017-06-24 23:52:02 -0500

    Fix error modal bug

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,7 +8,7 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to="/?location=SanDiego">San Diego, CA</Link>
+          <Link to="/?location=San Diego">San Diego, CA</Link>
         </li>
         <li>
           <Link to="/?location=Rio">Rio, Brazil</Link>
